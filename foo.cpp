85b907e4dd114f7d5c10455695b319a371456060
unknown <danzicr@EAS-BEN016T9.it.muohio.edu>
2014-09-11 11:08:25 -0400

    Added the foo.cpp file and changed main.cpp to reflect move of the foo function to its own file.

---
+++ foo.cpp
@@ -0,0 +1,6 @@
+#include "foo.h" //Technically not needed here
+
+// Definition of foo:
+int foo(int a, int b) {
+	return a + b + 2;
+}
--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@
 **/
 
 #include <iostream>
+#include "foo.h" //Quotes around file name MEANS look in same folder as this cpp file
+// Our error occurred when the file was not in the same folder as this cpp file.
+#include "helloworld.h"
 
 int main(void){
-	std::cout << "Hello World!" << std::endl;
+	
+	//std::cout << "Hello World!" << std::endl << foo(5, 7) << std::endl;
+
+	//Declared the instance of the class (called variable h) - do not have to use "new"
+	//as in Java - you can use the same syntax as "int a;"
+	helloworld h;
+				//take object h and call one of its methods (like myObject.doSomething();)
+	std::cout << h.doHelloWorld() <<std::endl;
 }
+
